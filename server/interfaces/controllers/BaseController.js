614577ac85a229d8ef45ca4b8398644cf1022f55
SlavaIvanchenko17 <torstein.viking.2020@gmail.com>
2021-05-23 20:06:47 +0300

    add repository loader

---
+++ server/interfaces/controllers/BaseController.js
@@ -0,0 +1,13 @@
+const repositories = require('../../infrastructure/repository');
+
+class BaseController{
+    constructor(){
+        this.repositories = repositories;
+        this.loadRepo();
+    }
+
+    loadRepo(){
+        this.repositories.forEach(Repository => new Repository()); 
+    }
+}
+
